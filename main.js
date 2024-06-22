9c1be5065cfecd3e33d4d91c1b33072d9b2d4585
Dalchand Yogi <129150745+Dalchandyogi@users.noreply.github.com>
2024-06-22 22:06:55 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function openContactBox() {
-    document.getElementById('contactBox').style.display = 'block';
-  }
-  
-  function closeContactBox() {
-    document.getElementById('contactBox').style.display = 'none';
-  }
-  
