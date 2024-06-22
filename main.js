551edd9be4968b944311e8fc4dd060941e42d255
Dalchand Yogi <129150745+Dalchandyogi@users.noreply.github.com>
2024-06-22 22:01:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function openContactBox() {
+    document.getElementById('contactBox').style.display = 'block';
+  }
+  
+  function closeContactBox() {
+    document.getElementById('contactBox').style.display = 'none';
+  }
+  
